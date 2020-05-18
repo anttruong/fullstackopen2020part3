82624209a8faf0f525f3eb69254472d80d87bb24
Anthony Truong <gonnahurttony@gmail.com>
2020-05-17 22:10:38 -0700

    3.2

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ app.use(express.json())
 app.use(express.static('build'))
 
 const persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": 1
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -40,7 +40,12 @@ app.get('/api/persons', (req, res) => {
   // })
 })
 
+app.get('/info', (req, res) => {
+  res.send(`<div>Phonebook has info for ${persons.length} people</div>
+  <div></br>${new Date}</div>`)
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
