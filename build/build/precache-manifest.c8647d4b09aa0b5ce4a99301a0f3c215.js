ad93da5b770dd83e0ded42c31f31664985fa0d2d
Anthony Truong <gonnahurttony@gmail.com>
2020-05-18 22:06:59 -0700

    push to heroku

---
+++ build/build/precache-manifest.c8647d4b09aa0b5ce4a99301a0f3c215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e67b224198c36e8e23ebf90bf5f390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce1dac21a3f8cb4359b6",
+    "url": "/static/css/main.e0618b94.chunk.css"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1dac21a3f8cb4359b6",
+    "url": "/static/js/main.91d2a26d.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
