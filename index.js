6fba33314b423976038f7b0f8d94ac6749141554
Anthony Truong <gonnahurttony@gmail.com>
2020-05-25 20:04:49 -0700

    3.15

--- index.js
+++ index.js
@@ -72,15 +72,23 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = People.find(person => person.id === id)
+  // const id = Number(req.params.id)
+  // const person = People.find(person => person.id === id)
 
-  if (person) {
-    persons = People.filter(person => person.id !== id)
-    res.sendStatus(204).end()
-  } else {
-    res.sendStatus(404).end()
-  }
+  // if (person) {
+  //   persons = People.filter(person => person.id !== id)
+  //   res.sendStatus(204).end()
+  // } else {
+  //   res.sendStatus(404).end()
+  // }
+
+  Person.findByIdAndRemove(req.params.id).then(result => {
+    res.status(204).end()
+  })
+    .catch(error => {
+      console.log(error)
+      Response.status(400).send({ error: 'asdf' })
+    })
 })
 
 app.get('/info', (req, res) => {
